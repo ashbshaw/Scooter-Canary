74a98d9a283b53fe4f6b2603de8a6c1925b8f6d2
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-07-04 12:36:14 -0500

    nav bootstrap

--- scooter/src/App.js
+++ scooter/src/App.js
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Search from "./pages/Search";
-import Nav from "./components/Nav";
+import Navs from "./components/Navs";
 import Footer from "./components/Footer";
 import Wrapper from "./components/Wrapper";
 import './App.css';
@@ -12,7 +12,7 @@ function App() {
   return (
     <Router>
       <div>
-        <Nav />
+        <Navs />
         <Wrapper>
           <Route exact path="/" component={Home} />
           <Route exact path="/about" component={About} />
---
+++ scooter/src/components/Navs/index.js
@@ -0,0 +1,72 @@
+import React from "react";
+import "./style.css"
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from 'reactstrap';
+
+class Navs extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <Navbar color='light' light expand='md'>
+          <NavbarBrand href='/'>Scooter Canary</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/about">About</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink id="nav-choice" href="/search">Search</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
+    )
+  }
+}
+
+export default Navs;
