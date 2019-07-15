fd5c4444296818807618e0c50d1cd5e5c82cf72b
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-07-14 20:55:15 -0500

    start backend

---
+++ backend/scooter.model.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+let Scooter = new Schema({
+    todo_description: {
+        type: String
+    },
+    todo_responsible: {
+        type: String
+    },
+    todo_priority: {
+        type: String
+    },
+    todo_completed: {
+        type: Boolean
+    }
+});
+
+module.exports = mongoose.model('Scooter', Scooter);
