019aa2e639b376072ca2968da1d5ebed86a76cd6
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-09-03 19:17:22 -0500

    mapwork

--- scooter/src/components/Map/index.js
+++ scooter/src/components/Map/index.js
@@ -1,78 +1,3 @@
-// import React from 'react';
-// import { GoogleApiWrapper, InfoWindow, Map, Marker } from 'google-maps-react';
-
-// // const API_KEY = process.env.REACT_APP_API_KEY;
-
-// class GoogleMap extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = {
-//       showingInfoWindow: false,
-//       activeMarker: {},
-//       selectedPlace: {},
-//     };
-
-//     // binding this to event-handler functions
-//     this.onMarkerClick = this.onMarkerClick.bind(this);
-//     this.onMapClick = this.onMapClick.bind(this);
-//   };
-
-//   onMarkerClick = (props, marker, e) => {
-//     this.setState({
-//       selectedPlace: props,
-//       activeMarker: marker,
-//       showingInfoWindow: true
-//     });
-//   };
-
-//   onMapClick = (props) => {
-//     if (this.state.showingInfoWindow) {
-//       this.setState({
-//         showingInfoWindow: false,
-//         activeMarker: null
-//       });
-//     };
-//   };
-
-//   render() {
-//     const style = {
-//       width: '50vw',
-//       height: '75vh',
-//       'marginLeft': 'auto',
-//       'marginRight': 'auto'
-//     };
-
-//     return (
-//       <Map
-//         item
-//         xs={12}
-//         style={style}
-//         google={this.props.google}
-//         onClick={this.onMapClick}
-//         zoom={14}
-//         initialCenter={{ lat: 39.0903317, lng: -94.57857 }}
-//       >
-//         <Marker
-//           onClick={this.onMarkerClick}
-//           title={'Marker'}
-//           position={{ lat: 39.0903317, lng: -94.57857 }}
-//           name={'Marker'}
-//         />
-//         <InfoWindow
-//           marker={this.state.activeMarker}
-//           visible={this.state.showingInfoWindow}
-//         >
-//         </InfoWindow>
-//       </Map>
-//     );
-//   };
-// };
-
-// export default GoogleApiWrapper({
-//   apiKey: 'AIzaSyDK2WcgF11p0u9xaCEew9Jmzg1yVpUAcIQ'
-// })(GoogleMap);
-
-
 import React from "react";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
@@ -138,6 +63,6 @@ class MapContainer extends React.Component {
 }
 
 export default GoogleApiWrapper({
-  apiKey: 'AIzaSyDK2WcgF11p0u9xaCEew9Jmzg1yVpUAcIQ',
+  apiKey: '',
   libraries: []
 })(MapContainer);
