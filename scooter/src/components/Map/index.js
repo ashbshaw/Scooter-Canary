d40e96fe5e71557960400cd4e5b879c355417181
ashbshaw <ashley.brooke.shaw@gmail.com>
2019-09-03 19:50:31 -0500

    no api for you

--- scooter/src/components/Map/index.js
+++ scooter/src/components/Map/index.js
@@ -1,56 +1,65 @@
-import React from "react"
-import { compose, withProps } from "recompose"
-import { withScriptjs, withGoogleMap, GoogleMap, Marker } from "react-google-maps";
+import React from "react";
+import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 
-// not sure where to put this
-// const API_KEY = process.env.REACT_APP_API_KEY;
+const API_KEY = process.env.REACT_APP_API_KEY;
 
-const MyMapComponent = compose(
-  withProps({
-    googleMapURL: "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places",
-    loadingElement: <div style={{ height: `100%` }} />,
-    containerElement: <div style={{ height: `400px` }} />,
-    mapElement: <div style={{ height: `100%` }} />,
-  }),
-  withScriptjs,
-  withGoogleMap
-)((props) =>
-  <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: 38.9717, lng: -95.2353 }}
-  >
-    {props.isMarkerShown && <Marker position={{ lat: 38.9717, lng: -95.2353 }} onClick={props.onMarkerClick} />}
-  </GoogleMap>
-);
-
-class Map extends React.PureComponent {
-  state = {
-    isMarkerShown: false,
+const MarkersList = props => {
+  const { locations, ...markerProps } = props;
+  return (
+    <span>
+      {locations.map((location, i) => {
+        return (
+          <Marker
+            key={i}
+            {...markerProps}
+            position={{ lat: location.lat(), lng: location.lng() }}
+          />
+        );
+      })}
+    </span>
+  );
+};
+class MapContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      locations: []
+    };
+    this.handleMapClick = this.handleMapClick.bind(this);
   }
-
-  componentDidMount() {
-    this.delayedShowMarker()
-  }
-
-  delayedShowMarker = () => {
-    setTimeout(() => {
-      this.setState({ isMarkerShown: true })
-    }, 3000)
-  }
-
-  handleMarkerClick = () => {
-    this.setState({ isMarkerShown: false })
-    this.delayedShowMarker()
-  }
-
+  handleMapClick = (ref, map, ev) => {
+    const location = ev.latLng;
+    this.setState(prevState => ({
+      locations: [...prevState.locations, location]
+    }));
+    map.panTo(location);
+  };
   render() {
+    const style = {
+      width: '50vw',
+      height: '75vh',
+      'marginLeft': 'auto',
+      'marginRight': 'auto'
+    };
     return (
-      <MyMapComponent
-        isMarkerShown={this.state.isMarkerShown}
-        onMarkerClick={this.handleMarkerClick}
-      />
-    )
+      <div className="map-container">
+        <Map
+          xs={12}
+          style={style}
+          google={this.props.google}
+          className={"map"}
+          zoom={this.props.zoom}   
+          onClick={this.handleMapClick}
+          initialCenter={{ lat: 39.0903317, lng: -94.57857 }}
+        >
+          <MarkersList locations={this.state.locations} icon="https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png" />
+        </Map>
+      </div>
+    );
   }
 }
 
-export default Map;
+export default GoogleApiWrapper({
+  apiKey: API_KEY,
+  libraries: []
+})(MapContainer);
